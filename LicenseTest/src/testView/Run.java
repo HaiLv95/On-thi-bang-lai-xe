888b5534891c21db5c7b0317732211a68d3337bc
Lê Văn Hai <hai95.lv@gmail.com>
2021-07-07 16:32:05 +0700

    code đăng nhập+ questionController

--- LicenseTest/src/testView/Run.java
+++ LicenseTest/src/testView/Run.java
@@ -1,14 +1,24 @@
 package testView;
 
+import testModel.User;
+
 /**
  *
  * @author hai95
  */
 public class Run {
     public  static frHome home;
+    public static dgLogin login;
+    public static User user;
     public static void main(String[] args) {
+        dgLogin();
+    }
+    public static void dgLogin(){
+        login = new dgLogin(Run.home, true);
+        login.setVisible(true);
+    }
+    public static void frHome(){
         home = new frHome();
         home.setVisible(true);
     }
-//    https://github.com/hailvph13040/On-thi-bang-lai-xe.git
 }
